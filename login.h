2103b21efc9482dd862554a0e2151fad3074b29f
gabrielaona21-design <gabrielaona21@gmail.com>
2026-01-16 21:56:24 -0500

    Create login.h
    Agrega estructura inicial de LoginWindow con slots de login y salida

---
+++ login.h
@@ -0,0 +1,26 @@
+#ifndef LOGINWINDOW_H
+#define LOGINWINDOW_H
+
+#include <QDialog>
+
+namespace Ui {
+class LoginWindow;
+}
+
+class LoginWindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit LoginWindow(QWidget *parent = nullptr);
+    ~LoginWindow();
+
+private slots:
+    void on_btnLogin_clicked();
+    void on_btnSalirLogin_clicked();
+
+private:
+    Ui::LoginWindow *ui;
+};
+
+#endif
